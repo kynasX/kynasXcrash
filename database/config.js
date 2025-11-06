31b57f426042c8e1d185dbc23f4df115fb8b5ca5
AustinOffc <achmuhlis77@gmail.com>
2025-11-06 09:04:45 +0700

    Add database/config.js

---
+++ database/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  tokens: "8589460980:AAHI9EEc-JtkQFrnmFezde5mPxWEttodBig",  // Ubah Jadi Token Bot Mu !!!
+  owner: "7215703677", // Ubah Jadi Id Mu !!!
+  port: "2213", // Ubah Jadi Port Panel Mu !!!
+  ipvps: "143.198.209.211" // Ubah Jadi Ip Vps Mu !!!
+};
